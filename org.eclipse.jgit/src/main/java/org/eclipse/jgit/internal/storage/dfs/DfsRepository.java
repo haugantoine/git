dee248f4916c86eec939909eb0aa9d85a9526b8d
antoine <antoine@antoine-PC>
2016-04-23 22:43:40 +0200

    refactor remove builder

--- org.eclipse.jgit/src/main/java/org/eclipse/jgit/internal/storage/dfs/DfsRepository.java
+++ org.eclipse.jgit/src/main/java/org/eclipse/jgit/internal/storage/dfs/DfsRepository.java
@@ -77,6 +77,7 @@ import org.eclipse.jgit.lib.RefDatabase;
 import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.ReflogReader;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.lib.SymbolicRef;
 import org.eclipse.jgit.lib.Ref.Storage;
@@ -101,20 +102,6 @@ public class DfsRepository extends Repository {
 	private final DfsRepositoryDescription description;
 
 	/**
-	 * Initialize a DFS repository.
-	 *
-	 * @param builder
-	 *            description of the repository.
-	 */
-	protected DfsRepository(DfsRepositoryBuilder builder) {
-		super(builder);
-		this.config = new DfsConfig();
-		this.description = builder.getRepositoryDescription();
-		objdb = new MemObjDatabase(this);
-		refdb = new MemRefDatabase();
-	}
-
-	/**
 	 * Initialize a new in-memory repository.
 	 *
 	 * @param repoDesc
@@ -122,7 +109,11 @@ public class DfsRepository extends Repository {
 	 * @since 2.0
 	 */
 	public DfsRepository(DfsRepositoryDescription repoDesc) {
-		this(new DfsRepositoryBuilder().setRepositoryDescription(repoDesc));
+		super(new RepositoryBuilder());
+		this.config = new DfsConfig();
+		this.description = repoDesc;
+		objdb = new MemObjDatabase(this);
+		refdb = new MemRefDatabase();
 	}
 
 	@Override
@@ -589,4 +580,5 @@ public class DfsRepository extends Repository {
 					b.getLeaf().getObjectId());
 		}
 	}
+
 }
